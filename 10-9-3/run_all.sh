459149e4b9dcef92de67e438a58d5911d10e3da6
emily <tsveetkova.aa@mipt.ru>
2025-12-09 17:31:07 +0300

    pics

--- 10-9-3/run_all.sh
+++ 10-9-3/run_all.sh
@@ -3,14 +3,14 @@
 echo "=== Starting Data Analysis Pipeline ==="
 
 echo "0. Cleaning old plots..."
-rm -f /home/emily/CLionProjects/plotter/*.png
+rm -f ../plotter/*.png
 
 echo "1. Running C++ data generator..."
 cd data_generator/
 ./run.sh
 
 echo "2. Copying data to plotter..."
-cp data/fwhm_results.txt ../plotter/data/
+cp data/results.csv ../plotter/data/
 
 # 4. Запускаем C# построение графиков
 echo "3. Running C# plotter..."
